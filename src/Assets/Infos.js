037b6478873f6f8a2a7255bbaa935897af40a8dd
prince-nkuru <nkuruprinces@gmail.com>
2024-07-28 22:23:02 +0530

    added data file

---
+++ src/Assets/Infos.js
@@ -0,0 +1,44 @@
+ const Data = [
+  {
+    question: 'which device is required for internet connection',
+    option1: 'Modem',
+    option2: 'Router',
+    option3: 'Lan cable',
+    option4: 'Pen drive',
+    ans:1,
+  },
+  {
+    question: 'which continent has higher number of countries',
+    option1: 'Asia',
+    option2: 'Africa',
+    option3: 'Europe',
+    option4: 'America',
+    ans: 4, 
+  },
+  {
+    question: 'computer can not boot if it does not have the?',
+    option1: 'Application software',
+    option2: 'internet',
+    option3: 'operating system',
+    option4: 'house',
+    ans:3,
+  },
+  {
+    question: 'first page of website is termed as',
+    option1: 'index page',
+    option2: 'homepage',
+    option3: 'sitemap',
+    option4: 'pen drive',
+    ans:2,
+  },
+  {
+    question: 'which university is better?',
+    option1: 'AUCA',
+    option2: 'UR',
+    option3: 'ULK',
+    option4: 'MOUNT KENYA',
+    ans:3,
+  }
+]
+
+export default Data;
