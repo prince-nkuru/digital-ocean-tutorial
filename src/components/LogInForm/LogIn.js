6a73e8a346ef93646a01ce737dfa8b07bb343733
prince-nkuru <nkuruprinces@gmail.com>
2024-08-29 21:24:52 +0530

    added functionality

--- src/components/LogInForm/LogIn.js
+++ src/components/LogInForm/LogIn.js
@@ -1,35 +1,31 @@
-import React, { useState } from "react"
-import './LogIn.css'
+// import React, { useState } from "react"
+// import './LogIn.css'
 
-const LogIn = () => {
-  const [action, setAction] = useState('sign Up');
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="text">{action}</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        {action === 'Login'? <div></div>: <div className="input-place">
-          <input placeholder="enter a name" type="text"/>
-        </div>}
+// const LogIn = () => {
+//   const [action, setAction] = useState('sign Up');
+//   return (
+//     <div className="container">
+//       <div className="header">
+//         <div className="text">{action}</div>
+//         <div className="underline"></div>
+//       </div>
+//       <div className="inputs">
+//         {action === 'Login'? <div></div>: <div className="input-place">
+//           <input placeholder="enter a name" type="text"/>
+//         </div>}
        
-        <div className="input-place">
-          <input placeholder="enter email" type="email"/>
-        </div>
-        <div className="input-place">
-          <input placeholder="enter password" type="password"/>
-        </div>
-      </div>
-      {action === 'Sign Up'? <div></div>:<div className="forgot">forgot password? <span>click here !</span></div> }
+//          <input placeholder="enter password" type="password"/>
+//         </div>
+//       </div>
+//       {action === 'Sign Up'? <div></div>:<div className="forgot">forgot password? <span>click here !</span></div> }
       
-      <div className="submit-group">
-        <div className= {action === 'Sign Up'? 'submit gray':'submit' } onClick={() => {setAction('Login')}}>Login</div>
-        <div className= {action === 'Login'? 'submit gray':'submit' } onClick={() => {setAction('Sign Up')}}>Sign Up</div>
-      </div>
-    </div>
-  )
-}
+//       <div className="submit-group">
+//         <div className= {action === 'Sign Up'? 'submit gray':'submit' } onClick={() => {setAction('Login')}}>Login</div>
+//         <div className= {action === 'Login'? 'submit gray':'submit' } onClick={() => {setAction('Sign Up')}}>Sign Up</div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default LogIn;
+// export default LogIn;
 
